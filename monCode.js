e8799ee2b728aa11487be3b75486d9398430061c
José GIL <jgil83000@gmail.com>
2023-01-06 11:00:40 +0100

    creation d'un fichier javascript et essai Task #1 - creer un fichier Javascript

--- monCode.js
+++ monCode.js
@@ -3,4 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
+function hello() {
+    alert("coucou");
+}
+window.addEventListener("load", function() {
+    window.document.querySelector("#btnHello").addEventListener("click", hello);
+});
 
+function validEnvoi() {
+    if (window.document.querySelector("#i_nom").value === "" &&
+    window.document.querySelector("#i_prenom").value === "" ) {
+    alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+    }
+    else {
+    window.document.querySelector("#form_contact").submit(); // On peut envoyer
+    }
+}
+window.addEventListener("load", function() {
+    window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+});
