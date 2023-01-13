a31bc47fd8cf5f811fe542725255dafc6f643f0e
ylemaire057@gmail.com <>
2023-01-13 11:50:50 +0100

    developpement des fonctions dans le fichier renumeration.js Task #3 - Creation et developpement de la page "simulateur.html" accompagnée du .js "remuneration.js"

---
+++ js/renumeration.js
@@ -0,0 +1,31 @@
+const fixe = 1100;
+
+function anciennete(ancien,salaire){
+    if()
+}
+
+function comS20(nb){
+    const prix_S20= 140;
+    const taux_S20= 0.02;
+    return nb*prix_S20*taux_S20;
+}
+
+function comXSpirit(nb){
+    const prixXSpirit = 350;
+    const tauxXSpirit = 0.06;
+    if (nb => 50){
+        return nb*prixXSpirit*tauxXSpirit;
+    }
+
+function comMultitec(nb){
+    const prixMultitec = 180;
+    if (nb => 20){
+        return nb*prixXSpirit*0.04;
+    }
+    else if (nb => 21, nb <=50){
+        return nb*prixXSpirit*0.06;
+    }
+    else{
+        return nb*prixXSpirit*0.10;
+    }
+}
