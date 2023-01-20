e39738c27f283b6948aaf1656828ab451f7f3582
Yannis Lemaire <lemairey83@gmail.com>
2023-01-20 11:52:16 +0100

    comprehension de la correction Task #4 - javascript nouvelle mise en pratique

--- js/renumeration.js
+++ js/renumeration.js
@@ -76,3 +76,110 @@ function recupComMulti(nb) {
         return ((nbMultiTranche1 * prixMu * txMultiTranche1) + ((nbMultiTranche2 - nbMultiTranche1) * prixMu * txMultiTranche2) + ((nb - nbMultiTranche2) * prixMu * txMultiTranche3));
     }
 }
+
+window.addEventListener("load", function() {
+ 
+    window.document.querySelector("#btn_calculer").addEventListener("click", function() {
+    // Déclaration des constantes
+    const fixe = 1100.0;
+
+    // Déclaration et affectation des variables
+    let nbAncien = parseInt(window.document.querySelector("#num_ancien").value);
+    let nbS20 = parseInt(window.document.querySelector("#num_s20").value);
+    let nbXS = parseInt(window.document.querySelector("#num_xs").value);
+    let nbMulti = parseInt(window.document.querySelector("#num_multi").value);
+    let remuneration = fixe + recupPrimeAnciennete(nbAncien, fixe)
+    + recupComS20(nbS20) + recupComXS(nbXS)
+    + recupComMulti(nbMulti);
+    // Affichage du résultat
+    window.document.querySelector("#remuneration").innerHTML =
+    "La rémunération sera de : " + remuneration + " €";
+    });
+});
+
+window.addEventListener("load", function() {
+    window.document.querySelector("#num_ancien").addEventListener("keyup", calcRemu);
+    window.document.querySelector("#num_s20").addEventListener("keyup", calcRemu);
+    window.document.querySelector("#num_xs").addEventListener("keyup", calcRemu);
+    window.document.querySelector("#num_multi").addEventListener("keyup", calcRemu);
+});
+
+window.addEventListener("load", function() {
+    // Déclaration de l'index de parcours
+    let i;
+    // tabInputs est une collection de <input>
+    let tabInputs = window.document.querySelectorAll("input");
+    // Parcours de tabInputs en s'appuyant sur le nombre de <input>
+    for (i=0 ; i < tabInputs.length ; i++) {
+    // Ajout d'un Listener sur tous les <input> sur l'évènement onKeyUp
+    tabInputs[i].addEventListener("keyup", calcRemu);
+    }
+});
+
+/**
+* Fonction principale qui s'occupe de récupérer les valeurs, calculer le montant
+* de la rémunération et qui s'occupe ensuite de l'afficher
+* 
+* @returns {undefined}
+*/
+function calcRemu() {
+    
+    // Déclaration des constantes
+    const fixe = 1100.0;
+    
+    // Déclaration et affectation des variables
+    let nbAncien = parseInt(window.document.querySelector("#num_ancien").value);
+    let nbS20 = parseInt(window.document.querySelector("#num_s20").value);
+    let nbXS = parseInt(window.document.querySelector("#num_xs").value);
+    let nbMulti = parseInt(window.document.querySelector("#num_multi").value);
+    let remuneration = fixe + recupPrimeAnciennete(nbAncien, fixe) + recupComS20(nbS20) + recupComXS(nbXS) + recupComMulti(nbMulti);
+    let km = recupValeur("#num_km");
+    let remuneration = fixe + recupPrimeAnciennete(nbAncien, fixe) + recupComS20(nbS20) + recupComXS(nbXS) + recupComMulti(nbMulti) + recupIndemKm(km);
+    
+    // Affichage du résultat
+    window.document.querySelector("#remuneration").innerHTML = "La rémunération sera de : " + remuneration + " €";
+}
+
+/**
+* Fonction qui retourne un entier depuis une valeur prise dans le DOM
+* 
+* @param {String} id
+* @return {integer}
+*/
+function recupValeur(id) {
+    return parseInt(window.document.querySelector(id).value);
+}
+
+// Affichage du résultat
+window.document.querySelector("#remuneration").innerHTML = "La rémunération sera de : " + remuneration + " €";
+
+// Affichage du résultat
+afficheRemu(remuneration);
+
+/**
+* Fonction qui affiche la rémunération dans l'élément d'id "remuneration"
+* 
+* @param {type} nombre
+* @return {undefined}
+*/
+function afficheRemu(nombre) {
+    window.document.querySelector("#remuneration").innerHTML =
+    "La rémunération sera de : " + nombre + " €";
+}
+
+/**
+* Fonction qui retourne l'indemnité kilométrique
+* @param {type} nb
+* @returns {float}
+*/
+function recupIndemKm(nb) {
+    const prix = 0.15, plafond = 350;
+    let indem = nb * prix;
+    if (indem > plafond) {
+    return plafond;
+    }
+    else {
+    return indem;
+    }
+}
+
