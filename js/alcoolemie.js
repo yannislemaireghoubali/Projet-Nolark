365e1c8eb7e0c01744acb9ef5f1f9343e69780e8
José GIL <jgil83000@gmail.com>
2023-02-03 11:40:12 +0100

    creation des premieres lignes de code Task #8 - Creations des premieres lignes de test

---
+++ js/alcoolemie.js
@@ -0,0 +1,17 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// 0 verre
+if (0 === getAlcooPur(0)) {
+    console.log('0 verre : ok');
+} else {
+    console.log('0 verre : erreur');
+}
+// 1 verre
+if (10 === getAlcooPur(1)) {
+    console.log('1 verre : ok');
+} else {
+    console.log('1 verre : erreur');
+}
