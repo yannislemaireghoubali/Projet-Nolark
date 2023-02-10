6e7c2bfa39e91c8abe4490adf1b171a11178b3c3
lemairey83@gmail.com <>
2023-02-10 09:50:52 +0100

    premier commit sur netbeans 12.6 Task #8 - Creations des premieres lignes de test

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -3,18 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
-// 0 verre
-if (0 === getAlcooPur(0)) {
-    console.log('0 verre : ok');
-} else {
-    console.log('0 verre : erreur');
-}
-// 1 verre
-if (10 === getAlcooPur(1)) {
-    console.log('1 verre : ok');
-} else {
-    console.log('1 verre : erreur');
-}
 
 /**
 * Tests unitaires du projet Alcoolémie
